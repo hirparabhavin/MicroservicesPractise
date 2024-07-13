0cc0e23db28305d7bcf67d9a6e3db5490c49bb6e
hirparabhavin <hirparabhavin@yahoo.com>
2024-07-13 12:05:44 +0530

    spring cloud config server
    spring cloud config server

---
+++ spring-cloud-config-server/spring-cloud-config-server/src/main/java/com/bhavinhirpara/microservices/spring_cloud_config_server/SpringCloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.bhavinhirpara.microservices.spring_cloud_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+
+}
