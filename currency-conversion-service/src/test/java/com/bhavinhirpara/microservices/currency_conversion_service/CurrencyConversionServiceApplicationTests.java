965f901aa4bbdfe9ef49733f548b95a12ad1b41e
hirparabhavin <hirparabhavin@yahoo.com>
2024-07-15 15:29:24 +0530

    added currency conversion service
    added currency conversion service

---
+++ currency-conversion-service/src/test/java/com/bhavinhirpara/microservices/currency_conversion_service/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavinhirpara.microservices.currency_conversion_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
